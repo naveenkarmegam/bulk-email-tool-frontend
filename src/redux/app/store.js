9b08e8a9222b93e26facdec03fa8d31550df2645
NAVEEN K <naveenkkn03@gmail.com>
2024-01-15 16:33:02 +0530

    store is updated

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -2,9 +2,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from '../global/userSlice'
+import functionalReducer from '../global/functionalSlice'
+import recipientsReducer from '../global/recipientsSlice'
+const userPersistConfig = {
+  key: 'user',
+  version: 1,
+  storage,
+};
 
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    user: persistedUserReducer,
+    functionality: functionalReducer,
+    recipients:recipientsReducer
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
@@ -14,3 +29,15 @@ const store = configureStore({
 const persistor = persistStore(store);
 
 export { store, persistor };
+
+
+// import { combineReducers } from '@reduxjs/toolkit';
+
+// import userReducer from '../global/userSlice';
+// import functionalReducer from '../global/functionalSlice';
+// import recipientsReducer from '../global/recipientsSlice'
+
+
+// const rootReducer = combineReducers({
+
+// });
